8a415286defb43286f21a81e03bc3fe292916c5b
zyanyato <zyannya.to@gmail.com>
2024-12-04 20:08:45 -0600

    hook

--- cypress.config.ts
+++ cypress.config.ts
@@ -14,7 +14,12 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3001',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+        log(message) {
+          console.log(message);
+          return null;
+        }
+      })
     },
   },
 });
